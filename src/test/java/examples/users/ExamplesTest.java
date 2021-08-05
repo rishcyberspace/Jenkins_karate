ea6b184cc49bfb6bc8911e1a80e0c43b2039912f
rishcyberspace <rishabh.r.gupta@accenture.com>
2021-08-05 20:17:42 +0530

    Adding Stable project

---
+++ src/test/java/examples/users/ExamplesTest.java
@@ -0,0 +1,39 @@
+package users;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import com.intuit.karate.junit5.Karate;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+class TestParallel {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:myfeatures").outputCucumberJson(true).parallel(10);
+        generateReport(results.getReportDir());
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+        
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
+        List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
+
+}
